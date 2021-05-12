0563664596fc91b67c14ae450952cd887a49f219
Anderson Pereira <andersononus@hotmail.com>
2021-05-12 19:47:15 -0300

    add mysql

---
+++ node/index.js
@@ -0,0 +1,46 @@
+
+const express = require('express')
+const faker = require('faker')
+const app = express()
+const port = 3001
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database:'nodedb'
+};
+const mysql = require('mysql')
+var connection = mysql.createConnection(config)
+
+//const sql = `INSERT INTO people(name) values('${faker.internet.userName}')`
+var sql = `CREATE TABLE IF NOT EXISTS nodedb.people (
+    id INT auto_increment NOT NULL,
+    name varchar(100) NULL,
+    PRIMARY KEY (id)
+)
+DEFAULT CHARSET=latin1
+COLLATE=latin1_swedish_ci;`
+
+connection.query(sql)
+connection.end()
+
+
+app.get('/', (req,res) => {
+    connection = mysql.createConnection(config)
+    sql = `INSERT INTO nodedb.people (name) VALUES('${faker.internet.userName()}');`
+    connection.query(sql)
+    connection.end()
+
+    connection = mysql.createConnection(config)
+    sql = `SELECT id, name FROM nodedb.people;`
+    const data = connection.query(sql, function (error, results, fields) {
+        if (error) throw error;
+        console.log('The solution is: ', results[0]);
+        res.send(`<h1>Full Cycle Rocks!</h1> <br /> ${results}`)
+      });
+    connection.end()
+})
+
+app.listen(port, ()=> {
+    console.log('Rodando na porta ' + port)
+})
