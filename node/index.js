d1622b4bca4664b5be8c563df9029dc4ed162eba
Anderson Pereira <andersononus@hotmail.com>
2021-05-12 20:33:36 -0300

    adjust file

--- node/index.js
+++ node/index.js
@@ -1,46 +1,46 @@
-
-const express = require('express')
-const faker = require('faker')
-const app = express()
-const port = 3001
+const express = require("express");
+const faker = require("faker");
+const app = express();
+const port = 3001;
 const config = {
-    host: 'db',
-    user: 'root',
-    password: 'root',
-    database:'nodedb'
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
 };
-const mysql = require('mysql')
-var connection = mysql.createConnection(config)
+const mysql = require("mysql");
+var connection = mysql.createConnection(config);
 
-//const sql = `INSERT INTO people(name) values('${faker.internet.userName}')`
 var sql = `CREATE TABLE IF NOT EXISTS nodedb.people (
     id INT auto_increment NOT NULL,
     name varchar(100) NULL,
     PRIMARY KEY (id)
 )
 DEFAULT CHARSET=latin1
-COLLATE=latin1_swedish_ci;`
+COLLATE=latin1_swedish_ci;`;
 
-connection.query(sql)
-connection.end()
+connection.query(sql);
+connection.end();
 
+app.get("/", (req, res) => {
+  connection = mysql.createConnection(config);
+  sql = `INSERT INTO nodedb.people (name) VALUES('${faker.internet.userName()}');`;
+  connection.query(sql);
+  connection.end();
 
-app.get('/', (req,res) => {
-    connection = mysql.createConnection(config)
-    sql = `INSERT INTO nodedb.people (name) VALUES('${faker.internet.userName()}');`
-    connection.query(sql)
-    connection.end()
+  connection = mysql.createConnection(config);
+  sql = `SELECT name FROM nodedb.people;`;
+  connection.query(sql, function (error, results, fields) {
+    if (error) throw error;
+    res.send(`<h1>Full Cycle Rocks!</h1> <br /> ${results.map((str) => {
+        const obj = JSON.parse(JSON.stringify(str))
+        return obj.name.replace(',', '') + '<br />';
+      })}`);
+  });
 
-    connection = mysql.createConnection(config)
-    sql = `SELECT id, name FROM nodedb.people;`
-    const data = connection.query(sql, function (error, results, fields) {
-        if (error) throw error;
-        console.log('The solution is: ', results[0]);
-        res.send(`<h1>Full Cycle Rocks!</h1> <br /> ${results}`)
-      });
-    connection.end()
-})
+  connection.end();
+});
 
-app.listen(port, ()=> {
-    console.log('Rodando na porta ' + port)
-})
+app.listen(port, () => {
+  console.log("Rodando na porta " + port);
+});
